3c85d364919d9bef0970082c746fc0d663189736
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-10-19 02:17:38 +0300

    add env.c

---
+++ _env.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _env - Function ptints environment command
+ *
+ * Return: function returns void.
+ */
+
+void _env(void)
+{
+	int i, j;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+			write(1, &environ[i][j], 1);
+		write(1, "\n", 1);
+	}
+}
