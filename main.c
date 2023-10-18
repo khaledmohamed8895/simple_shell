1616ed2504b6275841adab4a1495891bf8d4245a
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-10-19 01:52:49 +0300

    add main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Function that run my shell command-line interpreter .
+ * @argc: The number of command-line arguments.
+ * @argv: Array of strings containing the command-line arguments.
+ * Return: function returns (0).
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+
+	if (isatty(STDIN_FILENO) == 0)
+
+		_non_interactive_mode(argv);
+
+	else
+
+		_interactive_mode(argv);
+
+	return (0);
+}
